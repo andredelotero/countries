8c9702a9ce5b2ecfbdaedf2f14314522d908eb67
Andre Gama del Otero <andredelotero@gmail.com>
2022-01-27 14:33:29 -0300

    working solution to mini-css

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import { GetCountries } from "../../services/GetCountries";
 import { StyledSearch } from "./StyledSearch";
 
